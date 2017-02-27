147252d9746eedad5e9a5b54b7296b9ba0483679
MarcDussault <marc.dussault-matte.1@ens.etsmtl.ca>
2017-02-27 18:59:47 -0500

    modif partie marc
    .

---
+++ src/Arbre/NoeudTitreChapitre.java
@@ -0,0 +1,36 @@
+/******************************************************
+Cours :             ELE116
+Session :           Hiver 2017
+Groupe :            01
+Projet :            Laboratoire #2
+Étudiant(e)(s) :    Marc Dussault-Matte
+                    Hortense Houendji
+Code(s) perm. :     DUSM25029304
+                    HOUH31589002
+Professeur :        Kim Khoa Nguyen
+Nom du fichier :    NoeudTitreChapitre.java
+Date crée :         2017-02-13
+Date dern. modif.   2017-02-13
+*******************************************************
+Historique des modifications
+*******************************************************
+ 2017-02-13         Version initiale 
+*******************************************************/
+
+package Arbre;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoeudTitreChapitre extends Noeud
+{
+    private List<Noeud> Child = new ArrayList() ;
+    private Noeud Parent ;
+    private Object Data ;
+    
+    public NoeudTitreChapitre(Noeud parent, Object data)
+    {
+    	this.Parent = parent ;
+    	this.Data = data ;
+    }
+}
---
+++ src/Client/Client.java
@@ -0,0 +1,26 @@
+/******************************************************
+Cours :             ELE116
+Session :           Hiver 2017
+Groupe :            01
+Projet :            Laboratoire #2
+Étudiant(e)(s) :    Marc Dussault-Matte
+                    Hortense Houendji
+Code(s) perm. :     DUSM25029304
+                    HOUH31589002
+Professeur :        Kim Khoa Nguyen
+Nom du fichier :    Client.java
+Date crée :         2017-02-13
+Date dern. modif.   2017-02-13
+*******************************************************
+Historique des modifications
+*******************************************************
+ 2017-02-13         Version initiale 
+*******************************************************/
+
+package Client;
+
+public class Client {
+
+	
+	/*gestion des bouton*/
+}
